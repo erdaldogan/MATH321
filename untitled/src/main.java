236e640de4f75f3da11d1f8a4cff8b35e8a8fab9
erdal <erdalsidal99@gmail.com>
2019-11-26 22:53:40 +0300

    assignment-ii

---
+++ untitled/src/main.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class main {
+    public static void main(String[] args) throws IOException {
+        String[] arr = {"-p","input.txt"};
+        RegExp.main(arr);
+    }
+}
